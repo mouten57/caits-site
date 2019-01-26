054c24cdc039f6cf43a5785388b1b9d32c49639e
Matt Outen <mattoutenphysed@gmail.com>
2019-01-26 12:50:58 -0600

    first commit

---
+++ src/components/Banner.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Banner = props => (
+  <section id="banner" className="major">
+    <div className="inner">
+      <header className="major">
+        <h1>Hi, my name is Caitlin!</h1>
+      </header>
+      <div className="content">
+        <p>
+          Something will go here about how I, Caitlin Outen,
+          <br /> can totally improve your life for only 100 dollars!{' '}
+        </p>
+        <ul className="actions">
+          <li>
+            <a href="#one" className="button next scrolly">
+              See More
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+)
+
+export default Banner
