40e81297fa0ece7a93a9faa3f0f90ecd4fa5ded8
Matt Outen <mattoutenphysed@gmail.com>
2019-03-11 16:10:51 -0500

    import scss in gatsby browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom'
+import './src/assets/scss/material-kit-pro-react.scss?v=1.3.0'
 
 export const replaceHydrateFunction = () => (element, container, callback) =>
   ReactDOM.render(element, container, callback)
