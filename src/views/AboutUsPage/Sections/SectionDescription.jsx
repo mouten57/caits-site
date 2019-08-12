3b56069d95767be08f70e4cc426dda5d1f613b6e
matthew.outen <matthew.outen@matthew-outen-mbp.local>
2019-08-12 10:37:23 -0500

    style fix

--- src/views/AboutUsPage/Sections/SectionDescription.jsx
+++ src/views/AboutUsPage/Sections/SectionDescription.jsx
@@ -1,62 +1,57 @@
-import React from 'react'
+import React from 'react';
 // nodejs library that concatenates classes
-import classNames from 'classnames'
+import classNames from 'classnames';
 // core components
-import GridContainer from '../../../components/Grid/GridContainer.jsx'
-import GridItem from '../../../components/Grid/GridItem.jsx'
+import GridContainer from '../../../components/Grid/GridContainer.jsx';
+import GridItem from '../../../components/Grid/GridItem.jsx';
 // @material-ui/core components
-import withStyles from '@material-ui/core/styles/withStyles'
+import withStyles from '@material-ui/core/styles/withStyles';
 
-import descriptionStyle from '../../../assets/jss/views/aboutUsSections/descriptionStyle.jsx'
+import descriptionStyle from '../../../assets/jss/views/aboutUsSections/descriptionStyle.jsx';
 
-import aboutImage from '../../../assets/img/profilepic.jpeg'
+import aboutImage from '../../../assets/img/profilepic.jpeg';
 
 function SectionDescription(props) {
-  const { classes } = props
-  return (
-    <div className={classNames(classes.aboutDescription, classes.textCenter)}>
-      <GridContainer>
-        <GridItem xs={12} sm={6}>
-          <img
-            src={aboutImage}
-            alt="about"
-            style={{ maxWidth: '100%', paddingBottom: '20px' }}
-          />
-        </GridItem>
+	const { classes } = props;
+	return (
+		<div className={classNames(classes.aboutDescription, classes.textCenter)}>
+			<GridContainer>
+				<GridItem xs={12} sm={5} md={7} style={{ margin: '0 auto' }}>
+					<img src={aboutImage} alt="about" style={{ maxWidth: '100%', paddingBottom: '20px' }} />
+				</GridItem>
 
-        <GridItem xs={12} sm={6}>
-          <GridContainer
-            style={{
-              height: '90%',
-              flexDirection: 'column',
-              justifyContent: 'space-around',
-            }}
-          >
-            <GridItem>
-              <h1 className={classes.title}>About Me</h1>
-              <h4>Meet the founder of Caitlin May Consulting.</h4>
-            </GridItem>
-            <GridItem>
-              <p className={classes.description}>
-                My name is Caitlin and ever since I could remember I have been
-                passionate about helping others. It wasn’t until I became a
-                recruiter that I realized where my passion and skills would
-                meet.
-              </p>
-              <p className={classes.description}>
-                If you have worked with recruiters you probably have an opinion
-                about them, whether it be good or bad.
-              </p>
-              <p className={classes.description}>
-                But what a recruiter actually does seems to be the most
-                misunderstood topic for the average job seeker.
-              </p>
-            </GridItem>
-          </GridContainer>
-        </GridItem>
-      </GridContainer>
-    </div>
-  )
+				<GridItem xs={12} sm={12} md={5}>
+					<GridContainer
+						style={{
+							height: '90%',
+
+							justifyContent: 'space-around'
+						}}
+					>
+						<GridItem xs={12}>
+							<h1 className={classes.title}>About Me</h1>
+							<h4>Meet the founder of Caitlin May Consulting.</h4>
+						</GridItem>
+						<GridItem xs={12}>
+							<p className={classes.description}>
+								My name is Caitlin and ever since I could remember I have been passionate about helping
+								others. It wasn’t until I became a recruiter that I realized where my passion and skills
+								would meet.
+							</p>
+							<p className={classes.description}>
+								If you have worked with recruiters you probably have an opinion about them, whether it
+								be good or bad.
+							</p>
+							<p className={classes.description}>
+								But what a recruiter actually does seems to be the most misunderstood topic for the
+								average job seeker.
+							</p>
+						</GridItem>
+					</GridContainer>
+				</GridItem>
+			</GridContainer>
+		</div>
+	);
 }
 
-export default withStyles(descriptionStyle)(SectionDescription)
+export default withStyles(descriptionStyle)(SectionDescription);
